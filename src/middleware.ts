06d48bdee766a8ce193715d8a52586e73941774c
Jiha Ramdhan <jiharamdhan97@gmail.com>
2026-05-02 19:10:55 +0700

    feat: Auto Redirect when user still have token login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,25 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import withAuth from "./middleware/withAuth";
+import { getToken } from "next-auth/jwt";
 
-export function MainMiddleware(request: NextRequest) {
+export async function MainMiddleware(request: NextRequest) {
+    const { pathname } = request.nextUrl;
+
+    // get token buat cek user udah login apa belum
+    const token = await getToken({ 
+        req: request, 
+        secret: process.env.NEXTAUTH_SECRET 
+    });
+
+    // Jika user sudah ada token dan nyoba akses landing page (/) atau halaman auth
+    if (token) {
+        if (pathname.startsWith("/auth")) {
+            // arahin ke dashboard
+            return NextResponse.redirect(new URL("/dashboard", request.url));
+        }
+    }
+
     return NextResponse.next();
 }
 
